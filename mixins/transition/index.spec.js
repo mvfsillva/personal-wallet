c5e6086415aa6ff9cf12d5cf748ad03243c2c0c1
Marcus Silva <mvfsillva@gmail.com>
2019-04-26 17:16:54 -0300

    tests/mixins-transition (#15)
    * fix: resolve problem on import
    
    * refactor: move transition mixin helper to folder transition
    
    * tests: add transition specs

---
+++ mixins/transition/index.spec.js
@@ -0,0 +1,34 @@
+import { transitions } from 'polished'
+
+import transition from '.'
+
+describe('Helper: Transition', () => {
+  it('should verify of default transition', () => {
+    expect(transition()).toBe('all 150ms ease-out 0ms')
+  })
+
+  it('should verify of pure transition', () => {
+    expect(transition({ property: 'color' })).toBe('color 150ms ease-out 0ms')
+    expect(transition({ property: 'border-bottom', duration: '1s', delay: '700ms' })).toBe(
+      'border-bottom 1s ease-out 700ms',
+    )
+  })
+
+  it('should verify of transition with polished', () => {
+    expect(transitions(transition({ property: 'border-color' }))).toEqual({
+      transition: 'border-color 150ms ease-out 0ms',
+    })
+    expect(transitions(transition({ property: 'button', ease: 'ease-in' }))).toEqual({
+      transition: 'button 150ms ease-in 0ms',
+    })
+  })
+
+  it('should verify of array transitions with polished', () => {
+    expect(
+      transitions([
+        transition({ property: 'color' }),
+        transition({ property: 'background-color' }),
+      ]),
+    ).toEqual({ transition: 'color 150ms ease-out 0ms,background-color 150ms ease-out 0ms' })
+  })
+})
