27cf6ab5212fecd562b18ad2bc2ebfed34f2a147
Marcus Silva <mvfsillva@gmail.com>
2019-04-29 04:39:56 -0300

    feature/select-component (#37)
    * test: add select component specs
    
    * feat: add component select
    
    * docs: add component select stories
    
    * chore: add react-extras
    
    * feat: add arrow svg icons

---
+++ icons/arrow-down.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import theme from '../theme'
+
+const ArrowDown = ({ size, color }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 284.929 284.929"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path
+          fill={color}
+          d="M282.082,76.511l-14.274-14.273c-1.902-1.906-4.093-2.856-6.57-2.856c-2.471,0-4.661,0.95-6.563,2.856L142.466,174.441
+			L30.262,62.241c-1.903-1.906-4.093-2.856-6.567-2.856c-2.475,0-4.665,0.95-6.567,2.856L2.856,76.515C0.95,78.417,0,80.607,0,83.082
+			c0,2.473,0.953,4.663,2.856,6.565l133.043,133.046c1.902,1.903,4.093,2.854,6.567,2.854s4.661-0.951,6.562-2.854L282.082,89.647
+			c1.902-1.903,2.847-4.093,2.847-6.565C284.929,80.607,283.984,78.417,282.082,76.511z"
+        />
+      </g>
+    </svg>
+  )
+}
+
+ArrowDown.defaultProps = {
+  size: 24,
+  color: `${theme.colors.secondary}`,
+}
+
+ArrowDown.propTypes = {
+  size: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+}
+
+export default ArrowDown
---
+++ icons/arrow-up.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import theme from '../theme'
+
+const ArrowUp = ({ size, color }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 284.929 284.929"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path
+          fill={color}
+          d="M282.082,195.285L149.028,62.24c-1.901-1.903-4.088-2.856-6.562-2.856s-4.665,0.953-6.567,2.856L2.856,195.285
+		C0.95,197.191,0,199.378,0,201.853c0,2.474,0.953,4.664,2.856,6.566l14.272,14.271c1.903,1.903,4.093,2.854,6.567,2.854
+		c2.474,0,4.664-0.951,6.567-2.854l112.204-112.202l112.208,112.209c1.902,1.903,4.093,2.848,6.563,2.848
+		c2.478,0,4.668-0.951,6.57-2.848l14.274-14.277c1.902-1.902,2.847-4.093,2.847-6.566
+		C284.929,199.378,283.984,197.188,282.082,195.285z"
+        />
+      </g>
+    </svg>
+  )
+}
+
+ArrowUp.defaultProps = {
+  size: 24,
+  color: `${theme.colors.secondary}`,
+}
+
+ArrowUp.propTypes = {
+  size: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+}
+
+export default ArrowUp
