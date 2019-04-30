f0859636db30fe09d6b03ae5b6fc96a011884987
mvfsilva <mvfsillva@gmail.com>
2019-04-30 05:03:12 -0300

    refactor: add mercado-bitcoin endpoint

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-const { createServer } = require('http')
+const cacheableResponse = require('cacheable-response')
+const express = require('express')
 const next = require('next')
-const routes = require('./routes')
+const axios = require('axios')
 
-const config = require('./config')
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
 
-const app = next({ dev: process.env.NODE_ENV !== 'production' })
-const handler = routes.getRequestHandler(app)
-const port = config.port || 3000
+const handle = app.getRequestHandler()
 
-app.prepare().then(() =>
-  createServer(handler).listen(port, err => {
-    if (err) throw err
-
-    console.log(`> Ready on http://localhost:${port}`)
+const ssrCache = cacheableResponse({
+  ttl: 1000 * 60 * 60, // 1hour
+  get: async ({ req, res, pagePath, queryParams }) => ({
+    data: await app.renderToHTML(req, res, pagePath, queryParams),
   }),
-)
+  send: ({ data, res }) => res.send(data),
+})
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.get('/', (req, res) => ssrCache({ req, res, pagePath: '/' }))
+
+  server.get('/mercado-bitcoin', async (req, res) => {
+    const { ticker } = await axios
+      .get('https://www.mercadobitcoin.net/api/BTC/ticker')
+      .then(({ data }) => data)
+      .catch(error => console.error({ error }))
+    ssrCache({ req, res, pagePath: 'mercado-bitcoin' })
+    res.send({ ticker })
+  })
+
+  server.get('*', (req, res) => handle(req, res))
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
