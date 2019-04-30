809f34befbbc7e223c3adb4b9ef41305a0f410bb
marcus.silva@idwall.co <marcus.silva@idwall.co>
2019-04-29 22:38:10 -0300

    refactor: improve currency helper

--- helpers/currency-format/index.js
+++ helpers/currency-format/index.js
@@ -1,5 +1,17 @@
-const currencyFormat = (locale, currency, money) => {
-  return new Intl.NumberFormat(locale, { style: 'currency', currency })
+const currencyFormat = (coin, money) => {
+  const locale = {
+    brl: 'pt-BR',
+    bta: 'en-US',
+    btc: 'de-DE',
+  }
+
+  if (coin === 'bta') {
+    coin = 'usd'
+  }
+
+  const currency = coin.toUpperCase()
+
+  return new Intl.NumberFormat(locale[coin], { style: 'currency', currency })
     .format(money)
     .replace(/BTC/, 'Ƀ')
 }
