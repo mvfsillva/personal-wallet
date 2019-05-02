0182095f4783cb898c444f9ef04cd0c521c1107e
Marcus Silva <mvfsillva@gmail.com>
2019-05-02 03:25:02 -0300

    refactor: change logo :] (#51)

--- icons/wallet.js
+++ icons/wallet.js
@@ -5,14 +5,8 @@ import theme from '../theme'
 
 const PersonalWallet = ({ size, color }) => {
   return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 512 512"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="256" cy="256" r="251" stroke={color} strokeWidth="10" />
+    <svg width={size} height={size} viewBox="0 0 500 430" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill={color} d="M335.821 0H169.776L0 283.582L83.9552 429.105H419.776L500 283.582L335.821 0Z" />
     </svg>
   )
 }
