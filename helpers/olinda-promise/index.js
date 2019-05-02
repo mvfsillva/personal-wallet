7ee549a788d8163bd88e4832d32a91f5d6872031
Marcus Silva <mvfsillva@gmail.com>
2019-05-02 06:22:05 -0300

    fix: adjustment invalid Date on firefox (#52)

--- helpers/olinda-promise/index.js
+++ helpers/olinda-promise/index.js
@@ -6,22 +6,21 @@ import config from '../../config'
 const olindaPromise = async date => {
   const quoteDate = date || dateFns.format(new Date(), 'MM-DD-YYYY')
   let quotation = await axios
-    .get(
-      `${config.olindaApi}/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${quoteDate}'`,
-    )
+    .get(`${config.olindaApi}/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${quoteDate}'`)
     .then(res => res.data)
 
   if (quotation.value.length === 0) {
     if (dateFns.getDay(quoteDate) === 0) {
-      const datePlusOne = dateFns.addDays(quoteDate, 1)
+      const datePlusOne = dateFns.addDays(new Date(), 1)
       quotation = await olindaPromise(dateFns.format(datePlusOne, 'MM-DD-YYYY'))
 
       return quotation
     }
 
     if (dateFns.getDay(quoteDate) === 6 || dateFns.getDay(quoteDate) !== 0) {
-      const dateLessOne = dateFns.subDays(quoteDate, 1)
+      const dateLessOne = dateFns.subDays(new Date(), 1)
       quotation = await olindaPromise(dateFns.format(dateFns.subDays(dateLessOne, 1), 'MM-DD-YYYY'))
+
       return quotation
     }
   }
