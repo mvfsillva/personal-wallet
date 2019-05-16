b6d7444a110975859680958b378fd42cf2a21345
mvfsilva <marcusvinicius30798@gmail.com>
2019-05-16 17:14:03 -0300

    fix: solved problem with infinite loop

--- helpers/olinda-promise/index.js
+++ helpers/olinda-promise/index.js
@@ -11,16 +11,15 @@ const olindaPromise = async date => {
 
   if (quotation.value.length === 0) {
     if (dateFns.getDay(quoteDate) === 0) {
-      const datePlusOne = dateFns.addDays(new Date(), 1)
+      const datePlusOne = dateFns.addDays(quoteDate, 1)
       quotation = await olindaPromise(dateFns.format(datePlusOne, 'MM-DD-YYYY'))
 
       return quotation
     }
 
     if (dateFns.getDay(quoteDate) === 6 || dateFns.getDay(quoteDate) !== 0) {
-      const dateLessOne = dateFns.subDays(new Date(), 1)
+      const dateLessOne = dateFns.subDays(quoteDate, 1)
       quotation = await olindaPromise(dateFns.format(dateFns.subDays(dateLessOne, 1), 'MM-DD-YYYY'))
-
       return quotation
     }
   }
